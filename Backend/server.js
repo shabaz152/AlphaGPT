f631f42c2204ea96ff9260016407c42a362b50ee
apple <apple@apples-MacBook-Air.local>
2026-07-16 15:37:20 +0530

    Fix CORS for Amplify

--- Backend/server.js
+++ Backend/server.js
@@ -7,31 +7,31 @@ import authRoutes from "./routes/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 2000;
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://main.dc5b4rjixbysv.amplifyapp.com"
+];
+
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api", chatRoutes);
-app.use("/api/auth", authRoutes);
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDB connected successfully.");
-
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-
-    } catch (error) {
-       console.error("Database connection failed:");
-       console.error(error);
-    }
-};
-
-connectDB();
+app.use("/api/auth", authRoutes);   
 
 
 //       "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
